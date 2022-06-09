0ab035d01c489c5a5817680be0144040545ad45c
Daniel Frias <dfrias41@gmail.com>
2022-06-09 06:12:36 -0500

    Fixing texts

--- src/ABGGBA.cpp
+++ src/ABGGBA.cpp
@@ -4,7 +4,7 @@ bool ABGGBA::OnInit() {
 	// TODO: Load file config
 	m_pControlFrame = new ControlFrame("ABGGBA", wxPoint(50, 50), wxSize(300, 300));
 	m_pwfcAppConfig = std::make_shared<wxFileConfig>("ABGGBA", wxEmptyString, 
-														wxGetCwd() + L"\\ABGGBA.ini", wxEmptyString, wxCONFIG_USE_GLOBAL_FILE);
+													wxGetCwd() + L"\\ABGGBA.ini", wxEmptyString, wxCONFIG_USE_GLOBAL_FILE);
 	m_pControlFrame->AddConfig(m_pwfcAppConfig);
 	m_pControlFrame->Show(true);
 	return true;
